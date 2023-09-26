db1848dd06f66d59c3429a8f6a04730bbb78e3d2
Paul Sitoh <paulwizviz@gmail.com>
2023-09-26 18:00:31 +0100

    Update examples
    Include benchmarking for sqlite example 1
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- internal/sqlite/sqlite.go
+++ internal/sqlite/sqlite.go
@@ -4,43 +4,32 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
-	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 var (
-	ErrConn = errors.New("db connect error")
-	ErrStmt = errors.New("statement error")
+	ErrConn  = errors.New("db connect error")
+	ErrStmt  = errors.New("statement error")
+	ErrTable = errors.New("creating table error")
 )
 
-type Config struct {
-	ConnMaxIdleTime time.Duration
-	ConnMaxLifeTime time.Duration
-	MaxIdleConn     int
-	MaxOpenConn     int
-}
+const (
+	ver = "sqlite3"
+)
 
-func ConnectMem(config Config) (*sql.DB, error) {
-	db, err := sql.Open("sqlite3", ":memory:")
+func ConnectMemDefault() (*sql.DB, error) {
+	db, err := sql.Open(ver, ":memory:")
 	if err != nil {
 		return nil, fmt.Errorf("%w-%v", ErrConn, err)
 	}
-	db.SetConnMaxIdleTime(config.ConnMaxIdleTime)
-	db.SetConnMaxLifetime(config.ConnMaxLifeTime)
-	db.SetMaxIdleConns(config.MaxIdleConn)
-	db.SetMaxOpenConns(config.MaxOpenConn)
 	return db, nil
 }
 
-func CreateTable(db *sql.DB, strStmt string) error {
-	stmt, err := db.Prepare(strStmt)
+func ConnectMemPool(f string, maxPool int) (*sql.DB, error) {
+	db, err := sql.Open(ver, fmt.Sprintf("Data Source=%s;Pooling=True;Max Pool Size=%d", f, maxPool))
 	if err != nil {
-		return fmt.Errorf("%w-%v", ErrStmt, err)
+		return nil, fmt.Errorf("%w-%v", ErrConn, err)
 	}
-	_, err = stmt.Exec()
-	if err != nil {
-		return err
-	}
-	return nil
+	return db, nil
 }
--- internal/sqlite/sqlite_test.go
+++ internal/sqlite/sqlite_test.go
@@ -4,12 +4,23 @@ import (
 	"errors"
 	"fmt"
 	"testing"
-	"time"
 )
 
-func TestPrepareStmt(t *testing.T) {
+func createTable(input string) error {
+	db, err := ConnectMemDefault()
+	if err != nil {
+		return fmt.Errorf("%w-%s", ErrConn, err.Error())
+	}
+	_, err = db.Exec(input)
+	if err != nil {
+		return fmt.Errorf("%w-%s", ErrTable, err.Error())
+	}
+	return nil
+}
 
-	cases := []struct {
+// This test is used to verify CREATE TABLE syntax https://www.sqlite.org/lang_createtable.html
+func TestCreateTableStatement(t *testing.T) {
+	testcases := []struct {
 		input       string
 		want        error
 		description string
@@ -20,113 +31,37 @@ func TestPrepareStmt(t *testing.T) {
 			description: "Valid statement",
 		},
 		{
-			input:       "CREATE TABL IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)",
-			want:        ErrStmt,
-			description: "Invalid TABLE token",
+			input:       "CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname STRING, lastname TEXT)",
+			want:        nil,
+			description: "Replace TEXT with STRING",
+		},
+		{
+			input:       "CREATE TABLE IF NOT EXISTS people (INTEGER id, firstname STRING, lastname TEXT)",
+			want:        nil,
+			description: "Syntax err with type declaration",
+		},
+		{
+			input:       "CREAT TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)",
+			want:        ErrTable,
+			description: "Syntax err CREAT instead of CREATE",
+		},
+		{
+			input:       "CREATE TABLE IF NOT EXISTS people (PRIMARY KEY id INTEGER, firstname STRING, lastname TEXT)",
+			want:        ErrTable,
+			description: "Syntax err with constraint declaration",
 		},
 	}
 
-	for i, c := range cases {
-		db, err := ConnectMem(Config{})
-		if err != nil {
-			t.Fatalf(err.Error())
+	for i, tc := range testcases {
+		got := createTable(tc.input)
+		if tc.want == nil {
+			if tc.want != got {
+				t.Fatalf("Case: %d Description: %s Want: %v Got: %v", i, tc.description, tc.want, got)
+			}
+		} else {
+			if !errors.Is(got, ErrTable) {
+				t.Fatalf("Case: %d Description: %s Want: %v Got: %v", i, tc.description, tc.want, got)
+			}
 		}
-		got := CreateTable(db, c.input)
-		if c.want == nil {
-			if c.want != got {
-				t.Fatalf("Case: %d Description: %s Got: %v Want: %v", i, c.description, got, c.want)
-			}
-		} else if !errors.Is(got, c.want) {
-			t.Fatalf("Case: %d Description: %s Got: %v Want: %v", i, c.description, got, c.want)
-		}
-	}
-
-}
-
-func BenchmarkConn(b *testing.B) {
-	for idx, config := range []Config{
-		{
-			ConnMaxIdleTime: time.Duration(0),
-			ConnMaxLifeTime: time.Duration(0),
-			MaxIdleConn:     0,
-			MaxOpenConn:     0,
-		},
-		{
-			ConnMaxIdleTime: time.Duration(1 * time.Second),
-			ConnMaxLifeTime: time.Duration(1 * time.Second),
-			MaxIdleConn:     3,
-			MaxOpenConn:     3,
-		},
-	} {
-		b.Run(fmt.Sprintf("Scenario %d", idx), func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				ConnectMem(config)
-			}
-		})
-	}
-}
-
-func BenchmarkCreateTable(b *testing.B) {
-	for idx, c := range []Config{
-		{
-			ConnMaxIdleTime: time.Duration(0),
-			ConnMaxLifeTime: time.Duration(0),
-			MaxIdleConn:     0,
-			MaxOpenConn:     0,
-		},
-		{
-			ConnMaxIdleTime: time.Duration(1 * time.Second),
-			ConnMaxLifeTime: time.Duration(0),
-			MaxIdleConn:     0,
-			MaxOpenConn:     0,
-		},
-		{
-			ConnMaxIdleTime: time.Duration(0),
-			ConnMaxLifeTime: time.Duration(1 * time.Second),
-			MaxIdleConn:     0,
-			MaxOpenConn:     0,
-		},
-		{
-			ConnMaxIdleTime: time.Duration(0),
-			ConnMaxLifeTime: time.Duration(0),
-			MaxIdleConn:     4,
-			MaxOpenConn:     0,
-		},
-		{
-			ConnMaxIdleTime: time.Duration(1 * time.Second),
-			ConnMaxLifeTime: time.Duration(0),
-			MaxIdleConn:     4,
-			MaxOpenConn:     0,
-		},
-		{
-			ConnMaxIdleTime: time.Duration(0),
-			ConnMaxLifeTime: time.Duration(0),
-			MaxIdleConn:     0,
-			MaxOpenConn:     4,
-		},
-		{
-			ConnMaxIdleTime: time.Duration(0),
-			ConnMaxLifeTime: time.Duration(1 * time.Second),
-			MaxIdleConn:     0,
-			MaxOpenConn:     4,
-		},
-	} {
-		db, _ := ConnectMem(c)
-		b.Run(fmt.Sprintf("Scenario %d", idx), func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				var strStmts = []string{
-					"CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)",
-					"CREATE TABLE IF NOT EXISTS human (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)",
-					"CREATE TABLE IF NOT EXISTS girls (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)",
-					"CREATE TABLE IF NOT EXISTS boys (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)",
-					"CREATE TABLE IF NOT EXISTS animals (id INTEGER PRIMARY KEY, name TEXT)",
-					"CREATE TABLE IF NOT EXISTS dogs (id INTEGER PRIMARY KEY, name TEXT)",
-					"CREATE TABLE IF NOT EXISTS cats (id INTEGER PRIMARY KEY, name TEXT)",
-				}
-				for _, strStmt := range strStmts {
-					CreateTable(db, strStmt)
-				}
-			}
-		})
 	}
 }
