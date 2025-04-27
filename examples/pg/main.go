ade9509aa53f36e00bc37d39bd5c088b8b6df33e
Paul Sitoh <paulwizviz@gmail.com>
2025-04-27 20:31:49 +0100

    Add Postgres example
    - Add example to ping postgres DB
    - Add a share package for postgres Go utilities
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

---
+++ examples/pg/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"go-sql/internal/pg"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	db, err := pg.NewDB("postgres", "postgres", "localhost", 5432, "default")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	err = pg.Ping(db)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println("Pinging DB")
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go-sql
 go 1.18
 
 require (
+	github.com/lib/pq v1.10.9
 	github.com/mattn/go-sqlite3 v1.14.18
 	github.com/stretchr/testify v1.8.4
 )
